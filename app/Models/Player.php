d9d2061ed7cd6c625e793de3e8d827d13c3e83a2
ayoubjebb2001 <jebbouri1968@gmail.com>
2025-04-22 11:03:26 +0100

    initial comit

---
+++ app/Models/Player.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    protected $fillable = [
+        'position',
+        'foot',
+        'team_id'
+    ];
+
+    public function user(){
+        return $this->hasOne(User::class);
+    }
+}
